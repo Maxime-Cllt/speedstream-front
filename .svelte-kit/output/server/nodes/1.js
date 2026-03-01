6e3316ddfc8ddf85d1d437b79166e2d2247c6462
Maxime <98154358+Maxime-Cllt@users.noreply.github.com>
2026-03-01 12:16:24 +0100

    feat: enhance theme handling and clean up environment variables

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.B0wUn4tK.js","_app/immutable/chunks/DOw9hhkV.js","_app/immutable/chunks/ZSrVmk2I.js","_app/immutable/chunks/4HpQsPH8.js","_app/immutable/chunks/DiFDQoIQ.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.Bbbqxany.js","_app/immutable/chunks/DOw9hhkV.js","_app/immutable/chunks/ZSrVmk2I.js","_app/immutable/chunks/uPlyAR0U.js","_app/immutable/chunks/CfHxjR8V.js","_app/immutable/chunks/4HpQsPH8.js","_app/immutable/chunks/B15Wu3Bk.js"];
-export const stylesheets = [];
-export const fonts = [];
